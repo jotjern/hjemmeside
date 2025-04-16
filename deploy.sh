30b79663329053c8ea46f82d603cac56cf94592e
Jo Gramnæs Tjernshaugen <jotjernshaugen@gmail.com>
2025-04-16 20:44:59 +0200

    Improve polling for deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,13 +2,17 @@
 
 set -e
 
+git push
+
+sleep 5
+
 while true; do
   db_id=$(gh run list --commit "$(git rev-parse HEAD)" --json databaseId | jq '.[0].databaseId')
   if [ "$db_id" != "null" ]; then
     break
   fi
   echo "Waiting for run to appear..."
-  sleep 2
+  sleep 1
 done
 
-gh run watch "$db_id" --exit-status
+gh run watch -i 1 "$db_id" 
