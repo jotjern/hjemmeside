dd89ca6dea9852cba736b7c5a087437783bc1200
Jo Gramnæs Tjernshaugen <jotjernshaugen@gmail.com>
2025-04-16 20:47:16 +0200

    Make deploy script poll faster

--- deploy.sh
+++ deploy.sh
@@ -4,15 +4,17 @@ set -e
 
 git push
 
-sleep 5
+sleep 3
+
+echo "Waiting for the GitHub Action to start..."
 
 while true; do
   db_id=$(gh run list --commit "$(git rev-parse HEAD)" --json databaseId | jq '.[0].databaseId')
   if [ "$db_id" != "null" ]; then
     break
   fi
-  echo "Waiting for run to appear..."
+  echo "Still waiting for GitHub Action to start..."
   sleep 1
 done
 
-gh run watch -i 1 "$db_id" 
+gh run watch -i 0.25 "$db_id" 
